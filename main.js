13167da822daf9359ce520366f933af407daa53f
Bohdan <system.bla.16@gmail.com>
2023-05-17 21:17:42 +0300

    creating slick slider

---
+++ main.js
@@ -0,0 +1,8 @@
+// Enabling slick slider
+$(document).ready(function () {
+    $('.projects__wrapper').slick({
+        infinite: true,
+        slidesToShow: 3,
+        slidesToScroll: 3
+    })
+})
