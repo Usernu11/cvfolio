506a7e17cc36f69431fbe78e43352502195a2ba4
Bohdan <system.bla.16@gmail.com>
2023-05-19 18:10:13 +0300

    finished skills section

--- main.js
+++ main.js
@@ -1,9 +1,1 @@
-// $('.projects__wrapper').slick({
-//     infinite: true,
-//     slidesToShow: 3,
-//     slidesToScroll: 3,
-//     slidesToShow: 3,
-//     centerMode: true,
-//     accessibility: true
-//     // dots: true
-// });
+// 
