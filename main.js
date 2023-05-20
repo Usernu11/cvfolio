93219598dbd77f29c2878b8cec1d31f8310feb5d
Bohdan <system.bla.16@gmail.com>
2023-05-20 21:47:25 +0300

    added ivent listener for buttons and cards info

--- main.js
+++ main.js
@@ -1,13 +1,48 @@
-const menuButtons = document.querySelectorAll('.menu-button')
+const menu = document.querySelector('nav')
+const menuButtons = menu.querySelectorAll('div')
 
 // Adding an eventListener for menu buttons
 menuButtons.forEach(button => {
     button.addEventListener('click', () => {
+        const parrentNav = document.querySelector('nav')
+        const childNav = parrentNav.querySelector('.active')
+        const parrentDisplay = document.querySelector('.display')
+        const displayItems = parrentDisplay.querySelectorAll('.display-card')
+
         if (!button.classList.contains('active')) {
-            button.classList.add('active')
-            // remove old active class
-        } else {
-            console.log(true)
+            childNav.classList.remove('active')     // remove class 'active' from other el
+            button.classList.add('active')          // add class 'active' for new el
+        }
+
+        displayItems.forEach(el => {
+            if (!el.classList.contains('hidden')) {
+                el.classList.add('hidden')
+            }
+        })
+
+        switch (true) {
+            case button.classList.contains('menu__about'):
+                parrentDisplay.querySelector('.about').classList.remove('hidden')
+                break
+            case button.classList.contains('menu__skills'):
+                parrentDisplay.querySelector('.skills').classList.remove('hidden')
+                break
+            case button.classList.contains('menu__projects'):
+                parrentDisplay.querySelector('.projects').classList.remove('hidden')
+                break
+            case button.classList.contains('menu__exp'):
+                parrentDisplay.querySelector('.exp').classList.remove('hidden')
+                break
+            case button.classList.contains('menu__edu'):
+                parrentDisplay.querySelector('.edu').classList.remove('hidden')
+                break
+            case button.classList.contains('menu__hobbies'):
+                parrentDisplay.querySelector('.hobbies').classList.remove('hidden')
+                break
+            case button.classList.contains('menu__contacts'):
+                parrentDisplay.querySelector('.contacts').classList.remove('hidden')
+                break
         }
     })
 })
+
